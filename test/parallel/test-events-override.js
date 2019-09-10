f11139cf3e77e257406304fc6e52fc28b96886a1
Robert Nagy <ronagy@icloud.com>
2019-09-10 23:13:48 +0200

    fixup

--- test/parallel/test-events-override.js
+++ test/parallel/test-events-override.js
@@ -4,13 +4,53 @@ const common = require('../common');
 const { EventEmitter: EE } = require('events');
 
 {
-  const e = new EE();
-  e.on = common.mustCall();
-  e.addListener('test', () => {});
+  class A extends EE {};
+  A.prototype.on = common.mustCall();
+  new A().addListener('test', () => {});
 }
 
 {
-  const e = new EE();
-  e.removeListener = common.mustCall();
-  e.off('test', () => {});
+  class A extends EE {};
+  A.prototype.addListener = common.mustCall();
+  new A().on('test', () => {});
+}
+
+{
+  class A extends EE {};
+  A.prototype.removeListener = common.mustCall();
+  new A().off('test', () => {});
+}
+
+{
+  class A extends EE {};
+  A.prototype.off = common.mustCall();
+  new A().removeListener('test', () => {});
+}
+
+{
+  class A extends EE {};
+  A.prototype.off = common.mustNotCall();
+  A.prototype.on = common.mustCall();
+  class B extends A {};
+  B.prototype.off = common.mustCall();
+  new B().removeListener('test', () => {});
+  new B().addListener('test', () => {});
+}
+
+{
+  class A extends EE {};
+  A.prototype.removeListener = common.mustNotCall();
+  class B extends A {};
+  B.prototype.off = common.mustCall();
+  new B().removeListener('test', () => {});
+  new B().addListener('test', () => {});
+}
+
+{
+  class A extends EE {};
+  A.prototype.off = common.mustNotCall();
+  class B extends A {};
+  B.prototype.removeListener = common.mustCall();
+  new B().removeListener('test', () => {});
+  new B().addListener('test', () => {});
 }
