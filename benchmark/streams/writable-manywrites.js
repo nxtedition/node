b941d4211d197c16123dfbc311a2aec90dc7e714
Robert Nagy <ronagy@icloud.com>
2019-12-29 21:52:16 +0100

    stream: writable bitfield

--- benchmark/streams/writable-manywrites.js
+++ benchmark/streams/writable-manywrites.js
@@ -5,9 +5,9 @@ const Writable = require('stream').Writable;
 
 const bench = common.createBenchmark(main, {
   n: [2e6],
-  sync: ['yes', 'no'],
-  writev: ['yes', 'no'],
-  callback: ['yes', 'no']
+  // sync: ['yes', 'no'],
+  // writev: ['yes', 'no'],
+  // callback: ['yes', 'no']
 });
 
 function main({ n, sync, writev, callback }) {
